adca289932089b22365fa96ab5d39eefca9aa803
aureliobeo <marcosaureliovm@gmail.com>
2025-03-17 10:03:57 -0300

    Adicionando o comando de voz para abrir o chat e mundando a cor ao clicar no microphone

--- src/app/mavis/mavis.component.ts
+++ src/app/mavis/mavis.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, inject, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from "@angular/material/card";
@@ -20,55 +20,57 @@ import OpenAI from "openai";
 export class MavisComponent {
     messages: string[] = []; // Armazena as mensagens
     newMessage: string = ''; // Input da mensagem
+    isMicActive: Boolean = false;  // Controla a visibilidade do microfone
 
 
-    showChat = false;  // Controla a visibilidade do chat
+    showChat: Boolean = true;  // Controla a visibilidade do chat
     recognizing = false;  // Controla se a API de reconhecimento de voz está ativa
 
 
     // Inicialize a API de reconhecimento de fala
     recognition: any;
+    constructor(public crd: ChangeDetectorRef) {}
 
-    constructor() {
+    ngOnInit() {
         // Verifique se a API de reconhecimento de fala está disponível no navegador
         const SpeechRecognition = (window as any).SpeechRecognition ||
             (window as any).webkitSpeechRecognition;
         if (SpeechRecognition) {
             this.recognition = new SpeechRecognition();
             this.recognition.lang = 'pt-BR';  // Defina o idioma para português (Brasil)
-            this.recognition.start();
+            // this.recognition.start();
             this.recognition.continuous = true; // Para cada comando, precisa reiniciar o reconhecimento
             this.recognition.interimResults = false;
 
             // Ações ao reconhecer a fala
             this.recognition.onresult = (event: any) => {
-                const transcript = event.results[0][0].transcript.toLowerCase();
-                if (transcript.toLowerCase() == 'abra') {
-                    console.log(transcript.toLowerCase())
-                    this.showChat = true; // Abre o chat
+                // const transcript = event.results[0][0].transcript.toLowerCase();
+                const transcript = event.results[event.results.length - 1][0].transcript.trim();
+                if (transcript.toLowerCase() === 'abra') {
+                    this.openChat()
+                    this.crd.detectChanges();
                 } else {
                     this.messages.push(`Você: ${transcript}`);
                 }
-                this.newMessage = transcript;  // Coloca a fala reconhecida no campo de entrada
+                // this.newMessage = transcript;  // Coloca a fala reconhecida no campo de entrada
                 this.recognizing = true;
             };
 
-            // this.recognition.onstart = () => {
-            //     this.recognizing = true;
-            // };
+            this.recognition.onstart = () => {
+                this.recognizing = true;
+            };
 
-            // this.recognition.onend = () => {
-            //     this.recognizing = false;
-            // };
+            this.recognition.onend = () => {
+                this.recognizing = false;
+            };
 
-            // this.recognition.onerror = (event: any) => {
-            //     console.error('Erro no reconhecimento de fala: ', event.error);
-            //     this.recognizing = false;
-            // };
+            this.recognition.onerror = (event: any) => {
+                console.error('Erro no reconhecimento de fala: ', event.error);
+                this.recognizing = false;
+            };
         } else {
             console.error('Reconhecimento de fala não suportado no navegador.');
         }
-
     }
 
     // Método para enviar a mensagem
@@ -95,15 +97,29 @@ export class MavisComponent {
         }
     }
 
+    openChat() {
+        this.showChat = true;
+    }
+
     toggleChat() {
-        console.log(this.showChat)
         this.showChat = !this.showChat;
+        console.log(this.showChat)
         // this.showChat = !this.showChat;  // Alterna a visibilidade do chat
     }
     
     // Função que verifica se o input está vazio ou não
     isMessageEmpty() {
+        this.isMicActive = !this.isMicActive;
         return this.newMessage.trim().length === 0;
+    }
+
+    toggleMic() {
+        this.isMicActive = !this.isMicActive;
+        if (this.isMicActive) {
+            this.startVoiceRecognition();
+        } else {
+            this.recognition.stop();
+        }
     }
 
     // Inicia o reconhecimento de fala
